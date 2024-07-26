d592ed86b43028c36c09ec9a53329821650b528d
Bruno aka bruno <102603630+Bueurro@users.noreply.github.com>
2024-07-26 04:12:14 -0400

    refactor: Improve image loading performance on ProductPage

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -10,6 +10,8 @@ export default function ProductPage() {
     return (
         <section className='flex-grow flex items-center justify-center'>
             <h2 className='text-white'>Página de producto (hacer lógica) - ID: {idProducto}</h2>
+
+            {/* TODO: Esto queda feo aun así que ojito en si darse la flojera de hacerlo o borrarlo */}
             {product && <motion.img layoutId={uniqueLayoutId} alt="main product card" width={400} height={400} className="object-cover w-96 h-96 rounded-2xl" src={product.imagen} />}
         </section>
     );
