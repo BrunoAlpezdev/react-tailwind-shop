bb0bc6291dec14c0b8e0fde519aedb5a62d665c1
Bruno aka bruno <102603630+Bueurro@users.noreply.github.com>
2024-07-26 00:45:58 -0400

    add sample product list

---
+++ src/types/products.ts
@@ -0,0 +1,35 @@
+type products = {
+    imagen: string,
+    nombreProducto: string,
+    descripcion: string,
+    categoria: string
+}
+
+const productsList: products[] = [
+    {
+        imagen: 'https://falabella.scene7.com/is/image/Falabella/14950203_1?wid=800&hei=800&qlt=70',
+        nombreProducto: 'Polera Nike',
+        descripcion: 'desc',
+        categoria: 'destacado'
+    },
+    {
+        imagen: 'https://falabella.scene7.com/is/image/Falabella/15345455_1?wid=800&hei=800&qlt=70',
+        nombreProducto: 'Polera Puma',
+        descripcion: 'desc',
+        categoria: 'destacado'
+    },
+    {
+        imagen: 'https://http2.mlstatic.com/playera-gucci-varios-colores-y-modelos-hombre-envio-gratis-D_NQ_NP_857370-MLM26824870312_022018-F.jpg',
+        nombreProducto: 'Polera Gucci',
+        descripcion: 'desc',
+        categoria: 'destacado'
+    },
+    {
+        imagen: 'https://www.futuro.cl/wp-content/uploads/2019/07/slayer-polera-1024x1014.jpg',
+        nombreProducto: 'Polera Hedionda',
+        descripcion: 'desc',
+        categoria: 'destacado'
+    },
+]
+
+export default productsList
