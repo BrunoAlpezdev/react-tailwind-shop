bf641a9ce45233add8d038ca3434b320c4045c2b
Bruno aka bruno <102603630+Bueurro@users.noreply.github.com>
2024-07-31 11:17:21 -0400

    refactor: Update search button and input styles in CRUDPage

--- src/pages/CRUDPage.tsx
+++ src/pages/CRUDPage.tsx
@@ -7,8 +7,8 @@ export default function CRUDPage() {
   return (
     <div className="w-full p-4 overflow-x-auto">
       <section className="flex justify-center items-center mb-4 gap-5">
-        <button className="block p-1 rounded transition ease-in-out delay-75 bg-brand-light-accent text-brand-light-shades hover:bg-brand-light-accent-600"><SearchIcon width={28} height={28}/></button>
-        <input className="border-2 border-brand-light-accent px-2 py-1 rounded outline-none" type="text" />
+        <button className="block p-1 rounded transition ease-in-out delay-75 bg-brand-light-accent text-brand-light-shades hover:bg-brand-light-accent-600 dark:bg-brand-dark-accent dark:hover:bg-brand-dark-accent-600"><SearchIcon width={28} height={28}/></button>
+        <input className="border-2 border-brand-light-accent dark:border-brand-dark-accent px-2 py-1 rounded outline-none" type="text" />
         <button className='transition hover:scale-150 hover:border-teal-500'><CreateIcon width={30} height={30}/></button>
       </section>
 
