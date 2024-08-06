79632e8d1be1496760362f49ad60750da4567c9d
Bruno aka bruno <102603630+Bueurro@users.noreply.github.com>
2024-08-05 21:00:29 -0400

    refactor: Update import paths for components and pages

--- src/pages/base/NavbarComponent.tsx
+++ src/pages/base/NavbarComponent.tsx
@@ -1,15 +1,18 @@
 import '@styles/App.css'
-import { LightThemeIcon, DarkThemeIcon, BrandLogo } from '@components/IconsComponent';
-import { DropDownButton, DropDownProfile} from '@components/index';
-import { Link } from 'react-router-dom';
+import {
+    LightThemeIcon,
+    DarkThemeIcon,
+    BrandLogo
+} from '@components/IconsComponent'
+import { DropDownButton, DropDownProfile } from '@components/index'
+import { Link } from 'react-router-dom'
 import categoriesList from '@data/categoriesList'
-import useThemeHandler from 'src/hooks/useThemeHandler';
-import { useState } from 'react';
+import useThemeHandler from 'src/hooks/useThemeHandler'
+import { useState } from 'react'
 
 const NavbarComponent = () => {
-
-    const { theme, handleToggleTheme } = useThemeHandler();
-    const [openProfile, setOpenProfile] = useState<boolean>(false);
+    const { theme, handleToggleTheme } = useThemeHandler()
+    const [openProfile, setOpenProfile] = useState<boolean>(false)
 
     return (
         <header className="relative">
@@ -18,29 +21,54 @@ const NavbarComponent = () => {
             {/* Fin Sombra */}
 
             <section className="relative px-4 py-2 bg-brand-dark-shades text-brand-light-shades dark:bg-brand-main-700">
-                
                 <section className="flex justify-between items-center">
                     {/* item 1 */}
-                    <Link to='/'><BrandLogo /></Link>
+                    <Link to="/">
+                        <BrandLogo />
+                    </Link>
 
                     {/* item 2 */}
-                    <section className='flex gap-3'>
+                    <section className="flex gap-3">
                         {categoriesList.map((category) => (
-                            <DropDownButton key={category.idCategoria} nombreBoton={category.nombreBoton} categorias={category.categorias}/>
+                            <DropDownButton
+                                key={category.idCategoria}
+                                nombreBoton={category.nombreBoton}
+                                categorias={category.categorias}
+                            />
                         ))}
                     </section>
-                    <section className='flex gap-3 justify-between items-center'>
+                    <section className="flex gap-3 justify-between items-center">
                         <section>
-                            {/* item 3 */}
-                            {theme === 'light' 
-                            ?<button onClick={handleToggleTheme} className='transition hover:scale-110'><LightThemeIcon width={24} height={24} color='#EC9A94' /></button> 
-                            :<button onClick={handleToggleTheme} className='transition hover:scale-110'><DarkThemeIcon width={24} height={24} color='#FF3D3D' /></button>}
+                            {theme === 'light' ? (
+                                <button
+                                    onClick={handleToggleTheme}
+                                    className="transition hover:scale-110">
+                                    <LightThemeIcon
+                                        width={24}
+                                        height={24}
+                                        color="#EC9A94"
+                                    />
+                                </button>
+                            ) : (
+                                <button
+                                    onClick={handleToggleTheme}
+                                    className="transition hover:scale-110">
+                                    <DarkThemeIcon
+                                        width={24}
+                                        height={24}
+                                        color="#FF3D3D"
+                                    />
+                                </button>
+                            )}
                         </section>
-                        <section className=''>
-                            {
-                                openProfile && <DropDownProfile />
-                            }
-                            <button className='bg-blue-gray-500 p-1 rounded-sm w-fit' onClick={() => setOpenProfile((prev) => !prev)}>|||</button>
+                        {/* item 3 */}
+                        <section className="">
+                            {openProfile && <DropDownProfile />}
+                            <button
+                                className="bg-blue-gray-500 p-1 rounded-sm w-fit"
+                                onClick={() => setOpenProfile((prev) => !prev)}>
+                                |||
+                            </button>
                         </section>
                     </section>
                 </section>
@@ -49,4 +77,4 @@ const NavbarComponent = () => {
     )
 }
 
-export default NavbarComponent;
+export default NavbarComponent
