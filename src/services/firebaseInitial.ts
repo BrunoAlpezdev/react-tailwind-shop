cb46fc7cb297e0ef46271a218ed7b351718a1607
Bruno aka bruno <102603630+Bueurro@users.noreply.github.com>
2024-08-05 20:14:12 -0400

    refactor: Add Firebase initialization and utility functions
    This commit adds the Firebase initialization code and utility functions to the codebase. The `firebaseInitial.ts` file contains the necessary code to initialize the Firebase app, Firestore database, and authentication. It also includes functions to retrieve the Firebase app instance, Firestore database instance, and authentication instance. Additionally, there is a function to retrieve a collection from the Firestore database. This change enhances the functionality and integration of Firebase in the application.
    
    Co-authored-by: matias fuentes <matfuentesr@users.noreply.github.com>

---
+++ src/services/firebaseInitial.ts
@@ -0,0 +1,40 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { firebaseConfig } from "./firebaseconfig";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export function getFirebaseApp() {
+  return app;
+}
+
+export function getFirestoreDB() {
+  return db;
+}
+
+export function getAuthInstance() {
+  return auth;
+}
+
+export async function getCollection(collectionName: string) {
+  try {
+    const querySnapshot = await getDocs(collection(db, collectionName));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => obtained `);
+    });
+    const data = JSON.stringify(querySnapshot);
+    return data;
+    console.log("Data: ", data);
+  } catch (error) {
+    console.error("Error getting collection: ", error);
+  }
+}
+
+export function xd() {
+  console.log(firebaseConfig);
+}
